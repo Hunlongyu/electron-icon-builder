3ccf60ef2f59ebea9ffb00f4312206cfd575eb20
Hunlongyu <hunlongyu@gmail.com>
2022-06-29 23:26:48 +0800

    feat: 二进制文件 icon

--- src/build.rs
+++ src/build.rs
@@ -1,3 +1,10 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
 fn main() {
+  let mut res = winres::WindowsResource::new();
+  res.set_icon("src/ui/logo.ico");
+  res.compile().unwrap();
   slint_build::compile("src/ui/main.slint").unwrap();
 }
