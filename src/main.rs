95cabb5e1729f9dddacae5a982af315e206d3881
hunlongyu <hunlongyu@gmail.com>
2022-07-01 11:48:29 +0800

    feat: 完成 win ico 的功能

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // #![windows_subsystem = "windows"]
+mod utils;
 
-use std::{fs::File, env, path::Path, io::BufReader};
-use icns::{IconFamily, Image};
-use image::io;
 use slint::SharedString;
 use native_dialog::FileDialog;
+
+use crate::utils::utils::{ winico };
 
 slint::include_modules!();
 
@@ -54,16 +54,8 @@ fn main() {
 
     {
         main.on_handleConfirm(move |input: SharedString, output: SharedString, flag: bool| {
-            println!("handle confirm{} {} {}", input, output, flag);
-            let current_path = env::current_dir().unwrap();
-            let default_png_path = current_path.join("icon.png");
-            let mut png_path = Path::new("");
-            if input == "" {
-                png_path = &default_png_path;
-            } else {
-                png_path = &input;
-            };
-            println!("png path: {:?}", png_path);
+            println!("handle confirm: {} {} {}", input, output, flag);
+            winico(input, output, flag);
         });
     }
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,45 @@
 
 pub mod utils {
-  use std::env;
+  use std::{env, path::{PathBuf}, fs::{create_dir, create_dir_all}};
+  use slint::SharedString;
 
-  pub fn get_exe_path () -> String {
-    let current_path = match env::current_exe() {
-      Ok(exe_path) => {
-        exe_path.display().to_string()
-      }
-      Err(_) => panic!("Path does note exists"),
+  pub fn winico(input: SharedString, output: SharedString, is_flatten: bool) {
+    let current_dir = env::current_dir().unwrap();
+    println!("current dir: {:?}", current_dir);
+
+    let source_path;
+    if input == "" {
+      let default_png_path = current_dir.join("icon.png");
+      source_path = default_png_path;
+    } else {
+      let input_path = PathBuf::from(input.as_str());
+      source_path = input_path;
     };
-    current_path
+    println!("source path: {:?}", &source_path);
+
+    let target_path;
+    if output == "" {
+      if is_flatten {
+        create_dir(current_dir.join("build")).unwrap();
+        target_path = current_dir.join("icon.ico");
+      } else {
+        create_dir_all(current_dir.join("build").join("win")).unwrap();
+        target_path = current_dir.join("build").join("win").join("icon.ico");
+      };
+    } else {
+      let output_path = PathBuf::from(output.as_str());
+      if is_flatten {
+        create_dir(output_path.join("build")).unwrap();
+        target_path = output_path.join("icon.ico");
+      } else {
+        create_dir_all(output_path.join("build").join("win")).unwrap();
+        target_path = output_path.join("build").join("win").join("icon.ico")
+      };
+    };
+    println!("target path: {:?}", &target_path);
+
+    let png_file = image::open(source_path).unwrap().to_rgba8();
+    let resized = image::imageops::resize(&png_file, 256, 256, image::imageops::FilterType::Triangle);
+    resized.save(target_path).unwrap();
   }
 }
